18ffbea7894a7d020de2e9efe89ae74211637ae8
Todd Yu <toddyu@berkeley.edu>
2020-07-25 23:09:01 -0700

    added database and get route

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const connection = require('./database');
 
 app.get('/', (req, res) => res.send('hello world'));
 
-app.listen(3000, () => console.log('Example express app listening on port 3000...'));
+app.route('/users/:userId')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `userID` WHERE id = ? LIMIT 3", req.params.userId,
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+app.get('/status', (req, res) => res.send('Working!'));
+
+
+app.set('port', process.env.PORT || port);
+
+app.listen(port, () => console.log('listening on port 3000...'));
